280854f685b443554367bb52c09800e8a94fc8b5
Charlie Hanacek <charlie@czhanacek.com>
2017-11-11 11:44:09 -0800

    Bulk download of RSOs to csv

--- app/models/rso.rb
+++ app/models/rso.rb
@@ -7,4 +7,31 @@ class Rso < ApplicationRecord
   validates :nickname, :presence => true
   validates_length_of :nickname, :maximum => 50
 
+
+  def self.to_csv
+    attributes = ["Name", "Nickname", "Website", "Description", "Keyword 1", "Weight 1", "Keyword 2", "Weight 2", "Keyword 3", "Weight 3", "Keyword 4", "Weight 4", "Keyword 5", "Weight 5"]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      
+      all.each do |r|
+        row = []
+        #puts r.inspect
+        row << r.name
+        row << r.nickname
+        row << r.website
+        row << r.description
+        ks = r.keywords.limit(5)
+        puts ks.inspect
+        if(ks.empty? == false)
+          ks.each do |k|
+            row << k.keyword
+            row << k.weight
+          end
+        end
+        csv << row
+      end
+    end
+    
+  end
 end
